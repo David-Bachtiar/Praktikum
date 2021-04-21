c3d32eeab5a3af129db642c73ea8dc496f3c1547
David Bachtiar Simbolon <davidbachtiar91@gmail.com>
2021-04-21 14:18:12 +0700

    Menambahkan code penjumlahan

---
+++ bilangan.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+  using namespace std;
+  main()
+  {
+  int a,b;
+  cout << "masukkan angka pertama : ";
+  cin >> a;
+  cout << "masukkan angka kedua : ";
+  cin >> b;
+  cout<<"penjumlahan = " <<a + b << endl;
+  }
